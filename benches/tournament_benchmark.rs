1430c2b54a6b2e667d381e9b639072a88dc218e5
NSmeding <nick.smeding@alten.nl>
2025-09-16 10:21:22 +0100

    Added benchmark configs

--- benches/tournament_benchmark.rs
+++ benches/tournament_benchmark.rs
@@ -3,10 +3,18 @@ use rust_bomberman::{tournament, bot::available_bots};
 use rust_bomberman::tournament::prepare_bots;
 
 fn tournament_benchmark(c: &mut Criterion) {
-    c.bench_function("run_tournament", |b| {
+    let mut config = Criterion::default()
+        .sample_size(100) // default is 100; increase for more precision
+        .measurement_time(std::time::Duration::from_secs(120)) // default is 5s
+        .warm_up_time(std::time::Duration::from_secs(5)); // default is 3s
+
+
+    config.bench_function("run_tournament", |b| {
         b.iter(|| {
             let bots = available_bots();
             let game_bots = prepare_bots(&bots);
+
+
             tournament::run_game(game_bots);
         });
     });
