276a3a9c04484888abbd4ae6db96b29698b97ebe
NSmeding <nick.smeding@alten.nl>
2025-10-02 15:00:44 +0200

    apply gitignore

--- bots/target/debug/deps/bots-6025b768752e9372.d
+++
@@ -1,9 +0,0 @@
-C:\Repos\bomberman-rust-tournament\bomberman-rust-tournament\bots\target\debug\deps\bots-6025b768752e9372.d: src\lib.rs Cargo.toml
-
-C:\Repos\bomberman-rust-tournament\bomberman-rust-tournament\bots\target\debug\deps\libbots-6025b768752e9372.rmeta: src\lib.rs Cargo.toml
-
-src\lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
