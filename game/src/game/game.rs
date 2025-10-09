599be141d5ed4b8ed0907358a245cd5be8c3c20a
NSmeding <nick.smeding@alten.nl>
2025-10-09 15:33:02 +0200

    Fixed map size
    map size 5x5 was kinda unplayable because walls count with 4 players

--- game/src/game/game.rs
+++ game/src/game/game.rs
@@ -197,7 +197,11 @@ impl Game {
             } else {
                 // If no valid shrink location is found, we can handle it as needed.
                 // For now, we will just log an error or panic.
-                panic!("No valid shrink location found for turn {}", self.turn);
+                eprintln!("No valid shrink location found for turn {}", self.turn);
+                eprintln!("players {}", self.alive_players.len());
+                eprintln!("width {}", self.map.map_settings.width);
+                eprintln!("height {}", self.map.map_settings.height);
+                return true;
             }
 
             // set map location to wall
