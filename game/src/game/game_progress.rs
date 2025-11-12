ca2cf45925f9ed606a13f4213478acd2880f7e61
Martijn L <martijn.lankhof@topicus.nl>
2025-11-12 10:06:49 +0100

    Updated grid factory to start with celltype P

--- game/src/game/game_progress.rs
+++
@@ -1,5 +0,0 @@
-#[allow(dead_code)]
-pub struct GameProgress {
-    pub turn: usize,
-    pub endgame_started: bool,
-}
--- game/src/game/mod.rs
+++ game/src/game/mod.rs
@@ -1,4 +1,3 @@
-pub mod game_progress;
 pub mod game_result;
 pub mod game;
 pub mod replay_engine;
